bb707a52eeeb152f14694766af6321b11f4dc986
Yuan Yao <yy682@cornell.edu>
2020-02-04 10:00:21 -0500

    .

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -5,6 +5,8 @@ A python package for the MSF 2020A Bootcamp.
 
 # Add imports here
 from .functions import *
+from .atom_data import atomic_weights, atom_colors
+from .io import *
 
 # Handle versioneer
 from ._version import get_versions
