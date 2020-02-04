d3ef2f4710e0c2ba8c1cad45342e335153d8dfef
Yuan Yao <yy682@cornell.edu>
2020-02-04 11:03:01 -0500

    improve open_pdb func

--- molecool/molecool_io/pdb.py
+++ molecool/molecool_io/pdb.py
@@ -4,12 +4,15 @@ def open_pdb(f_loc):
     # This function reads in a pdb file and returns the atom names and coordinates.
     with open(f_loc) as f:
         data = f.readlines()
-    c = []
+
+    coordinates = []
     sym = []
-    for l in data:
-        if 'ATOM' in l[0:6] or 'HETATM' in l[0:6]:
-            sym.append(l[76:79].strip())
-            c2 = [float(x) for x in l[30:55].split()]
-            c.append(c2)
-    coords = np.array(c)
-    return sym, coords
+    for line in data:
+        if 'ATOM' in line[0:6] or 'HETATM' in line[0:6]:
+            sym.append(line[76:79].strip())
+            atom_coordinates = [float(x) for x in line[30:55].split()]
+            coordinates.append(atom_coordinates)
+
+    coordinates = np.array(coordinates)
+
+    return sym, coordinates
