1696de5ad7248aaf09d5a5d454adbf47f9a23b59
Yuan Yao <yy682@cornell.edu>
2020-02-04 10:34:58 -0500

    group funcs into modules

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -6,7 +6,11 @@ A python package for the MSF 2020A Bootcamp.
 # Add imports here
 from .functions import *
 from .atom_data import atomic_weights, atom_colors
-from .io import *
+from .measure import calculate_distance, calculate_angle
+from .molecule import build_bond_list
+from .visualize import bond_histogram, draw_molecule
+
+import molecool.molecool_io as io
 
 # Handle versioneer
 from ._version import get_versions
